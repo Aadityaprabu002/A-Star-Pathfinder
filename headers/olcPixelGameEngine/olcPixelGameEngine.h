ed7d3cefffe0e5bb3307ae8f8109e1bd14b88778
Saisathish Karthikeyan <71921104+ksaisathish@users.noreply.github.com>
2022-06-16 19:25:19 +0530

    Add files via upload

--- headers/olcPixelGameEngine/olcPixelGameEngine.h
+++ headers/olcPixelGameEngine/olcPixelGameEngine.h
@@ -801,9 +801,9 @@ namespace olc
 	class Renderable
 	{
 	public:
-		Renderable() = default;		
-		Renderable(Renderable&& r) : pSprite(std::move(r.pSprite)), pDecal(std::move(r.pDecal)) {}		
-		Renderable(const Renderable&) = delete;
+		Renderable() = default;
+		Renderable(Renderable&& r)  : pSprite(std::move(r.pSprite)), pDecal(std::move(r.pDecal))  {}
+		Renderable(const Renderable&)  = delete;
 		olc::rcode Load(const std::string& sFile, ResourcePack* pack = nullptr, bool filter = false, bool clamp = true);
 		void Create(uint32_t width, uint32_t height, bool filter = false, bool clamp = true);
 		olc::Decal* Decal() const;
