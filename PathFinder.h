1204b392535252cb0b3206ea94cdd9da54007cc5
Saisathish Karthikeyan <71921104+ksaisathish@users.noreply.github.com>
2022-06-16 19:24:19 +0530

    Add files via upload

--- PathFinder.h
+++ PathFinder.h
@@ -1,9 +1,6 @@
 #include "olcPixelGameEngine\olcPixelGameEngine.h"
 #include<iostream>
-#include<unordered_set>
 #include<vector>
-#include<math.h>
-#include<time.h>
 using namespace std;
 
 class Pathfinder : public olc::PixelGameEngine
@@ -17,14 +14,12 @@ private:
 		double g;
 		double f;
 		bool obstacle;
-		bool visited;
 		node* parent;
-
 		vector<node*> neighbours;
 		vector<node*> neighbours_wd;//Neighbours Without Diagonals
 		node(int x = 0, int y = 0) {
 			this->obstacle = false;
-			this->visited = false;
+			
 			this->parent = NULL;
 			this->h = 0.0;
 			this->g = 0.0;
@@ -37,7 +32,6 @@ private:
 			return false;
 		}
 	};
-
 
 	int ofs_x;//Offset x
 	int ofs_y;//Offset y
@@ -57,17 +51,53 @@ private:
 	vector<node*> ClosedSet;
 	vector<node*> SolvedPath;
 	vector<node*> SolvedPath_wd;
-	double Heuristics(node* Neighbour, node* End);
-	bool OnUserCreate() override;
-	void FindPath(vector<node*> &SP, float &PL, bool &PF,int choice);
-	//choice Values : 1 - A* with Diagonals, 2 - A* Without Diagonal
 	int flag, flag1;//For Menu Buttons
-	void PrintPath(vector<node*>& SP, float& PL, olc::Pixel Color,float LW);//LW-Line Width
+
+
 	bool OnUserUpdate(float fElapsedTime) override;
+
+	bool OnUserCreate() override;
+
+
+	/*
+	Heuristics is a function used to guess the minimum distance between two nodes. 
+	It takes in one neighbour and the end node and returns the tentative distance between them.
+	*/
+	double Heuristics(node* Neighbour, node* End);
+
+	/*
+	Find Path Function implements the A* Algorithm.
+	It takes in parameters SolvedPath(SP), PathLength(PL), and PathFound(PF) and choice of path.
+	Choice Values : 1 for Path with Diagonals, 2 for Path Without Diagonal
+
+	*/
+	void FindPath(vector<node*> &SP, float &PL, bool &PF,int choice); 
+	
+	/*
+	PrintTitleBar Function is used to display title bar of the graphical interface.
+	*/
 	void PrintTitleBar();
+
+	/*
+	PrintMenu function is used to display the Menu system of the graphical interface.
+	*/
 	void PrintMenu();
+	
+	/*
+	PrintPath function is used to Print the path in the Graph/Grid.
+	It takes in parameters SolvedPath(SP), PathLength(PL), Color of Pixel(Color) and Line Width(LW).
+	
+	*/
+	void PrintPath(vector<node*>& SP, float& PL, olc::Pixel Color,float LW); 
+	
+	
 public:
-	Pathfinder(int, int, float,int,int,int,int);
+	/*
+	This is the Pathfinder class constructor.
+	It takes in parameters: Number of grid rows(GridRows), Number of Grid columns (GridCols), ObstaclePopulation,
+	Graph Width (GWidth),Graph Height(GHeight),Offset X(ofs_x), Offset Y(ofs_y).
+	*/
+	Pathfinder(int GridRows, int GridCols , float ObstaclePopulation, int GWidth , int GHeight, int ofs_x , int ofs_y);
 	~Pathfinder();
 };
 
